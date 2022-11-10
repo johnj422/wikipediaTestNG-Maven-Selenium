32ef1ad0d8dae4c11d67c01496b5349267a34901
john.arango <johnj42@gmail.com>
2022-11-10 12:32:03 -0500

    Driver class set

--- src/main/java/org/wikipedia/configuration/Driver.java
+++ src/main/java/org/wikipedia/configuration/Driver.java
@@ -1,4 +1,32 @@
 package org.wikipedia.configuration;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Locale;
+
 public class Driver {
+
+    private WebDriver driver;
+
+    public Driver(String browser) {
+
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+        }
+
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
 }
