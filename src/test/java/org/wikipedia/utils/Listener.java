25eeda093535cd570b70bf28bec618a085d245c1
john.arango <johnj42@gmail.com>
2022-11-10 12:46:36 -0500

    Listener class set

--- src/test/java/org/wikipedia/utils/Listener.java
+++ src/test/java/org/wikipedia/utils/Listener.java
@@ -1,4 +1,19 @@
 package org.wikipedia.utils;
 
-public class Listener {
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.wikipedia.reporting.Reporter;
+
+public class Listener implements ITestListener {
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        Reporter.info("Test " + result.getName() + "[PASSED]");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        Reporter.error("Test " + result.getName() + "[FAILED]");
+    }
+
 }
