54be3cb225f66d84ea8243e477705d6403e39e29
john.arango <johnj42@gmail.com>
2022-11-10 15:30:58 -0500

    BasePage class set

--- src/test/java/org/wikipedia/pages/BasePage.java
+++ src/test/java/org/wikipedia/pages/BasePage.java
@@ -1,4 +1,16 @@
 package org.wikipedia.pages;
 
-public class BasePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.wikipedia.configuration.WebOperations;
+
+public class BasePage extends WebOperations {
+
+    @FindBy(id = "mw-head")
+    private WebElement topBar;
+
+    public BasePage(WebDriver driver) {
+        super(driver);
+    }
 }
--- src/test/java/org/wikipedia/tests/SearchArticlePage.java
+++ src/test/java/org/wikipedia/tests/SearchArticlePage.java
@@ -1,4 +1,5 @@
 package org.wikipedia.tests;
 
 public class SearchArticlePage {
+
 }
