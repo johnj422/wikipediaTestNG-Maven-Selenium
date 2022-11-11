2d3fdc67b48e1a060a486f4345f723d1184436b5
john.arango <johnj42@gmail.com>
2022-11-10 19:49:05 -0500

    ArticlePage elements set

--- src/test/java/org/wikipedia/pages/ArticlePage.java
+++ src/test/java/org/wikipedia/pages/ArticlePage.java
@@ -1,10 +1,34 @@
 package org.wikipedia.pages;
 
+import java.util.List;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class ArticlePage extends BasePage{
 
+    @FindBy(css = "#firstHeading span")
+    private WebElement articleTitle;
+
+    @FindBy(css = ".listaref ol li")
+    private List<WebElement> references;
+
+    @FindBy(className = "onfobox")
+    private WebElement sideInfoBox;
+
     public ArticlePage(WebDriver driver) {
         super(driver);
+    }
+
+    public String getArticleTitle() {
+        return articleTitle.getText();
+    }
+
+    public int getAmountOfReferences() {
+        return references.size();
+    }
+
+    public boolean sideInfoBoxIsDisplayed() {
+        return sideInfoBox.isDisplayed();
     }
 }
--- src/test/java/org/wikipedia/tests/SearchArticleTest.java
+++ src/test/java/org/wikipedia/tests/SearchArticleTest.java
@@ -1,5 +1,11 @@
 package org.wikipedia.tests;
 
+import org.testng.annotations.Test;
+import org.wikipedia.pages.HomePage;
+
 public class SearchArticleTest extends BaseTest {
+
+    @Test
+
 
 }
